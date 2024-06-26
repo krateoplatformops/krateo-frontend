51917ddc25bcdbd7ad6a1a1434ff94bd832cbceb
codev99 <code.v99@gmail.com>
2024-06-26 14:39:38 +0200

    feat: 81 wip notifications components draft

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -1,38 +1,38 @@
-import { useEffect, useState } from "react";
-import { getBaseUrl } from "../../utils/api";
-import RichRow from "../Widgets/RichRow/RichRow";
-import Button from "../Widgets/Button/Button";
+// import { useEffect, useState } from "react";
+// import { getBaseUrl } from "../../utils/api";
+// import RichRow from "../Widgets/RichRow/RichRow";
+// import Button from "../Widgets/Button/Button";
 
-type EventType = {
-  uid: string,
-  type: string,
-  icon: string,
-  color: string,
-  primaryText: string,
-  secondaryText: string,
-  subPrimaryText: string,
-  subSecondaryText: string,
-}
+// type EventType = {
+//   uid: string,
+//   type: string,
+//   icon: string,
+//   color: string,
+//   primaryText: string,
+//   secondaryText: string,
+//   subPrimaryText: string,
+//   subSecondaryText: string,
+// }
 
 const Notification = () => {
-  const [events, setEvents] = useState<EventType[]>([]);
+  // const [events, setEvents] = useState<EventType[]>([]);
 
-  useEffect(() => {
-    // opening a connection to the server to begin receiving events from it
-    const eventSource = new EventSource(`${getBaseUrl()}/notifications`);
+  // useEffect(() => {
+  //   // opening a connection to the server to begin receiving events from it
+  //   const eventSource = new EventSource(`${getBaseUrl()}/notifications`);
     
-    // attaching a handler to receive message events
-    eventSource.onmessage = (event) => {
-      const newEvents = JSON.parse(event.data);
-      setEvents({ ...newEvents, ...events });
-    };
+  //   // attaching a handler to receive message events
+  //   eventSource.onmessage = (event) => {
+  //     const newEvents = JSON.parse(event.data);
+  //     setEvents({ ...newEvents, ...events });
+  //   };
     
-    // terminating the connection on component unmount
-    return () => eventSource.close();
-  }, [events]);
+  //   // terminating the connection on component unmount
+  //   return () => eventSource.close();
+  // }, [events]);
 
   return (
-    <>Notification</>
+    <></>
     // events.map(el => <RichRow key={el.uid} {...el} />)
   )
 }
