a055dc27490f9322896d81e2e663bfd436cf97d0
codev99 <58194031+codev99@users.noreply.github.com>
2024-12-09 15:12:18 +0100

    feat: endpoint to create menu (#137)
    * feat: 135 dynamic menu creation
    
    * feat: 135 routes creation optimization and default route handle
    
    * feat: 135 InitRoutes component and refactoring
    
    * feat: 135 eslint fixes

---
+++ src/components/InitRoutes/InitRoutes.tsx
@@ -0,0 +1,118 @@
+import { useCallback, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import { Navigate, RouteObject, useNavigate } from "react-router-dom"
+import { logout, selectLoggedUser, setUser } from "../../features/auth/authSlice";
+import { useLazyGetContentQuery } from "../../features/common/commonApiSlice";
+import { useAppDispatch } from "../../redux/hooks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Page from "../Page/Page";
+import Layout from "../Layout/Layout";
+import ErrorPage from "../../pages/ErrorPage";
+import Profile from "../../pages/Profile";
+import { Result, Space, Spin, Typography } from "antd";
+
+const InitRoutes = ({updateRoutes}: {updateRoutes: (routes: RouteObject[]) => void}) => {
+  const isRoutesUpdated = useRef<boolean>(false);
+  const user = useSelector(selectLoggedUser);
+  const userLS = JSON.parse(localStorage.getItem("K_user") ||"{}");
+  const [getContent, {data, isSuccess, isLoading, isFetching, isError}] = useLazyGetContentQuery();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const getRoutes = useCallback(async () => {
+    const ls = localStorage.getItem("K_config") || "{}";
+    const configJson = JSON.parse(ls);
+    if ( configJson?.api?.INIT) {
+      await getContent({ endpoint: configJson.api.INIT })
+    }
+  }, [])
+
+  // Check user logged
+  useEffect(() => {
+    if (user) {
+      if (!isRoutesUpdated.current) {
+        // get all routes
+        getRoutes()
+      }
+    } else {
+      if (userLS.data) {
+        dispatch(setUser(userLS));
+      } else {
+        // user not logged!
+        navigate("/login");
+      }
+    }
+  }, [user])
+
+  useEffect(() => {
+    if (isSuccess && data) {
+      isRoutesUpdated.current = true;
+      if (data?.status?.type?.toLowerCase() === "menu") {
+        const routesList = data.status.items?.map(el => (
+          {
+            label: el.status.items[0].app.label,
+            path: el.status.items[0].app.path,
+            icon: <FontAwesomeIcon icon={el.status.items[0].app.icon} />,
+            endpoint: el.status.items[0].actions ? el.status.items[0].actions[0]?.path : undefined,
+            menu: el.status.items[0].app.menu,
+          }
+        ))
+
+        const routes:RouteObject[] = routesList.map((r) => (
+          {
+            path: r.path !== "/" ? r.path : undefined,
+            index: r.path === "/",
+            element: <Page endpoint={r.endpoint} />,
+          }
+        ));
+
+        let newRoutes = [
+          {
+            path: "/",
+            element: <Layout menu={routesList.filter(el => el.menu.toLowerCase() === "true")} />,
+            errorElement: <ErrorPage />,
+            children: [...routes, {
+              path: "/profile",
+              element: <Profile />
+            } ],
+          }
+        ]
+
+        // add default page
+        if (data.status.default) {
+          newRoutes = [{
+            path: "/",
+            element: <Navigate to={data.status.default} replace={true} />,
+            errorElement: <ErrorPage />,
+            children: []
+          }, ...newRoutes]
+        }
+
+        updateRoutes(newRoutes)
+      }
+    }
+  }, [data, isSuccess])
+
+  useEffect(() => {
+    if (isError && data.data.status === 401) {
+      // invalid user logged
+      dispatch(logout());
+      navigate("/login");
+    }
+  })
+
+  return (
+    isLoading || isFetching ?
+      <Space direction="vertical" size="large" style={{width: '100%', height: '100vh', alignItems: 'center', justifyContent: 'center'}}>
+        <Spin size="large" />
+        <Typography.Text>Krateo loading app data...</Typography.Text>
+      </Space>
+    :
+    isError ?
+      <Result status="error" title="Ops! Something didn't work" subTitle="Unable to retrieve routes data" />
+    :
+    <></>
+  )
+}
+
+export default InitRoutes
