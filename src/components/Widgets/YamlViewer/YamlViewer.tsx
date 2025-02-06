bdf5f9e149c133c5f5048426b99fafaa6bd3be9e
fedepini <federico.pini@krateo.io>
2025-02-06 11:22:46 +0100

    feat: updated props

--- src/components/Widgets/YamlViewer/YamlViewer.tsx
+++ src/components/Widgets/YamlViewer/YamlViewer.tsx
@@ -2,7 +2,7 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import { lightfair } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import yaml from 'js-yaml';
 
-const YamlViewer = ({ json }: { json: Record<string, any> }) => {
+const YamlViewer = ({ json }: { json: string }) => {
   const yamlString = yaml.dump(json, { indent: 2 });
   
   return (
